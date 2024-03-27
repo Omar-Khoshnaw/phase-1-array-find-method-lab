245044d3e1cdbf798e4ec7aa40bdd3ac6bd454f0
Omer Sardar <omar.khoshnaw@gmail.com>
2024-03-27 08:00:30 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+function superbowlWin(arr) {
+  const recObj = arr.find((i) => (i.result === "W" ? i.year : undefined));
+
+  if (recObj) {
+    return recObj.year;
+  } else {
+    return undefined;
+  }
+}
